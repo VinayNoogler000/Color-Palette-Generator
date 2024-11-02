b1a196ed5d5a9bc64a7f536a57208a6c4d078615
VinayNoogler000 <vinaytambey000@gmail.com>
2024-11-02 19:30:36 +0530

    Feat - Display the hex-code of a specific color el's bg-color, on 'mouseenter' event.

--- src/app.js
+++ src/app.js
@@ -1,7 +1,17 @@
+class colorEl { //to create objects of color elements in the "color-palette" el, by following DRY approach. 
+    constructor(el, hexCode) {
+        this.el = el;
+        this.hexCode = (hexCode == undefined ? el.querySelector(".hex-code").innerText : hexCode);
+    }
+}
+
 //Selecting HTML DOM Elements:
 const genBtnEl = document.querySelector("#generateBtn");
 const colorPaletteEl = document.querySelector(".color-palette");
-const colorElsArr = [document.querySelector("#color1"), document.querySelector("#color2"), document.querySelector("#color3"), document.querySelector("#color4")];
+const colorElsArr = [new colorEl(colorPaletteEl.querySelector("#color1")), 
+                     new colorEl(colorPaletteEl.querySelector("#color2")), 
+                     new colorEl(colorPaletteEl.querySelector("#color3")),
+                     new colorEl(colorPaletteEl.querySelector("#color4"))];
 
 // Function to Convert a Decimal number to a Hexadecimal:
 const decimalToHexadecimal = (decimalNum) => {
@@ -22,44 +32,81 @@ const decimalToHexadecimal = (decimalNum) => {
 }
 
 // Function to Convert RGB Color Code to Hex Color Code:
-const rgbToHexCode = (rgbColor) => {
-    return "#" + decimalToHexadecimal(rgbColor[0]) + decimalToHexadecimal(rgbColor[1]) + decimalToHexadecimal(rgbColor[2]);
+const rgbToHexCode = (rgbColorArr) => {
+    return "#" + decimalToHexadecimal(rgbColorArr[0]) + decimalToHexadecimal(rgbColorArr[1]) + decimalToHexadecimal(rgbColorArr[2]);
 }
 
-// Function to Generate a Random Color in "RGB" format:
+// Function to Generate a Random Color in "Hexadecimal" format:
 const generateRandomColor = () => {
     //below variables👇, represents RED, GREEN & BLUE colors ranging(0-255), in RGB Color Format:
     let red = Math.floor(Math.random() * 256); 
     let green = Math.floor(Math.random() * 256); 
     let blue = Math.floor(Math.random() * 256);
     
-    // let rgbColor = [red, green, blue];  //stores randomly generated color in RGB format 
-    return `rgb(${red}, ${green}, ${blue})`; 
+    let rgbColor = [red, green, blue];
+    return rgbToHexCode(rgbColor); 
+}
+
+const hideHexCodes = () => {
+    for (let colorObj of colorElsArr) {
+        const hexCodeEl = colorObj.el.querySelector(".hex-code");
+        hexCodeEl.classList.add("hidden");
+    }
+}
+
+//Function to disable the "generate button", until the random colors generation is stopped:
+const disableGenBtn = (value) => {
+    if(value) {
+        genBtnEl.disabled = value;
+        genBtnEl.classList.remove("btn-hover-effect");
+        genBtnEl.classList.replace("text-white", "text-slate-300");
+        genBtnEl.classList.replace("bg-sky-800", "bg-sky-900"); 
+    }   
+    else {
+        genBtnEl.disabled = value;
+        genBtnEl.classList.add("btn-hover-effect"); 
+        genBtnEl.classList.replace("text-slate-300", "text-white");
+        genBtnEl.classList.replace("bg-sky-900", "bg-sky-800"); 
+    }
 }
 
 // Function to Update the "Color Palette Element" by Displaying Random Colors, at every 350ms interval:
 const updateColorPalette = () => {
-    let i = 0; //index to iterate elements in 'colorElsArr[]'
+    isGeneratingColors = true;
+    disableGenBtn(true);
+    hideHexCodes();
 
+    let i = 0; //index to iterate elements in 'colorElsArr[]'
+    
     intervalId = setInterval( () => {
-        if(i > 3) { //condition, to reset the 'i' index
+        if(i > 3) { //condition, to reset the 'i' index, when 'i' is more than last idx of 'colorElsArr[]'
             i = 0; 
         }
-        colorElsArr[i].style.backgroundColor = generateRandomColor(); //update the Color-Element's bg-color
+
+        let randomColor = generateRandomColor(); 
+        colorElsArr[i].el.style.backgroundColor = colorElsArr[i].hexCode = randomColor;
         i++; //increment the 'i' index
     }, 350);
 }
 
-let intervalId; //stores the ID of the "setInterval()" used in the "udpateColorPalette()"  
-genBtnEl.addEventListener("click", updateColorPalette);
-colorPaletteEl.addEventListener("mouseenter", () => {
-    // Stop Generating & Displaying Random Colors in the Color-Palette:
+const stopGeneratingColors = () => {
     clearInterval(intervalId);
-});
+    isGeneratingColors = false;
+    disableGenBtn(false);
+}
 
-// Loop to apply "mouseover" event to all the "Color-Elements" in the Color-Palette:
-colorElsArr.forEach( (el) => {
-    el.addEventListener("mouseenter", () => {
-        console.log(el);
-    });
-});
+const displayHexCode = (colorEl, hexCode) => {
+    const hexCodeEl = colorEl.querySelector(".hex-code");
+    hexCodeEl.classList.remove("hidden");
+    hexCodeEl.textContent = hexCode;
+}
+
+let intervalId; //stores the ID of the "setInterval()" used in the "udpateColorPalette()"
+let isGeneratingColors = false;  
+genBtnEl.addEventListener("click", updateColorPalette);
+colorPaletteEl.addEventListener("mouseenter", stopGeneratingColors);
+
+// Loop to apply "mouseover" event handler to all the "Color-Elements" in the Color-Palette:
+for (let colorObj of colorElsArr) {
+    colorObj.el.addEventListener("mouseenter", () => displayHexCode(colorObj.el, colorObj.hexCode));
+}
